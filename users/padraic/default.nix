816f1bc221473f59dfef8ee2cfe6f0d8a91ce3fb
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2020-12-21 00:05:05 +0000

    add git-crypt with secrets file

--- users/padraic/default.nix
+++ users/padraic/default.nix
@@ -1,10 +1,16 @@
 { config, lib, pkgs, ... }:
+let
+  inherit (builtins) toFile readFile;
+  inherit (lib) fileContents mkForce;
 
-{
+  name = "Pádraic Ó Mhuiris";
+in {
   users.users.padraic = {
     uid = 1000;
     isNormalUser = true;
+    description = name;
     group = "users";
+    hashedPassword = fileContents ../../secrets/padraic;
     extraGroups = [ "wheel" "audio" "networkmanager" "video" ];
   };
 
