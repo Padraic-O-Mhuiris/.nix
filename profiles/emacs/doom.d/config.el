1713af4c87231a2f50b959b0272da4f2eb71c978
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-01-07 16:38:47 +0000

    emacs config

--- profiles/emacs/doom.d/config.el
+++ profiles/emacs/doom.d/config.el
@@ -10,3 +10,14 @@
 (setq org-roam-db-location "~/.org/org-roam.db")
 
 (pinentry-start)
+
+(setq tab-width 2)
+
+;; Javascript settings
+(setq javascript-indent-level 2)
+(setq typescript-indent-level 2)
+(setq web-mode-code-indent-offset 2)
+
+(setq-hook! 'js2-mode-hook +format-with-lsp nil)
+(setq-hook! 'typescript-mode-hook +format-with-lsp nil)
+(setq-hook! 'typescript-tsx-mode-hook +format-with-lsp nil)
