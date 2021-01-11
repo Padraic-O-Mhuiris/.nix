6572b6b10f8143b98a4e34e6ad3296527e4bcee9
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-01-11 11:20:26 +0000

    updates

--- profiles/emacs/doom.d/config.el
+++ profiles/emacs/doom.d/config.el
@@ -18,30 +18,12 @@
 
 (setq-hook! 'js2-mode-hook +format-with-lsp nil)
 (setq-hook! 'typescript-mode-hook +format-with-lsp nil)
-(setq-hook! 'typescript-tsx-mode-hook +format-with-lsp nil
+(setq-hook! 'typescript-tsx-mode-hook +format-with-lsp nil)
 
 ;; Org
 (setq org-directory "~/.org")
 (setq org-roam-directory "~/.org")
 (setq org-roam-db-location "~/.org/org-roam.db")
 (setq org-archive-location "::* HISTORY")
-
-(after! org
-  (setq org-todo-keywords
-        '((sequence "TODO"
-                    "MEETING"
-                    "PHONE"
-                    "PURCHASE"
-                    "CLARIFY"
-                    "MAYBE"
-                    "WAITING"
-                    "POSTPONED"
-                    "ROUTINE"
-                    "BLOCKED"
-                    "|"
-                    "DONE"
-                    "UNABLE"
-                    "REJECTED"
-                    "PURCHASED"
-                    "INACTIVE"
-                    "CANCELLED"))))
+(setq org-agenda-files (directory-files-recursively "~/.org/" "\\.org$"))
+(setq org-roam-tag-sources '(prop vanilla all-directories))
