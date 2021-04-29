3bcd8f75ce3135fdb0b1c685f470a5c5baacaacb
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-29 23:44:25 +0100

    removed editor

--- profiles/git/default.nix
+++ profiles/git/default.nix
@@ -6,7 +6,6 @@
       enable = true;
       userName = "Padraic-O-Mhuiris";
       userEmail = "patrick.morris.310@gmail.com";
-      editor = "emacs";
       signing = {
         key = "0xBD01159F2C44F16B";
         signByDefault = true;
