cec2a7eaaed71bcb834628b8f22c37602a8bec06
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-04-29 23:42:44 +0100

    removed

--- profiles/git/default.nix
+++ profiles/git/default.nix
@@ -11,9 +11,7 @@
         key = "0xBD01159F2C44F16B";
         signByDefault = true;
       };
-      extraConfig = { http = { postBuffer = "524288000"; };
-                      credential = { helper = "cache"; };
-                    };
+      extraConfig = { http = { postBuffer = "524288000"; }; };
     };
 
   };
