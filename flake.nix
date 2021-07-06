4b21a4d29f4dee8daba4e043f7470cba36e762fd
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-07-06 15:02:49 +0100

    cece

--- flake.nix
+++ flake.nix
@@ -58,8 +58,8 @@
           my = self.packages."${system}";
         };
 
-      # overlays =
-      #   mapModules ./overlays import;
+      overlays =
+        mapModules ./overlays import;
 
       # packages."${system}" =
       #   mapModules ./packages (p: pkgs.callPackage p {});
