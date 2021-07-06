2827288350279faf0a4477cb4c84f10c07bb3e90
Padraic-O-Mhuiris <patrick.morris.310@gmail.com>
2021-07-06 17:57:20 +0100

    ece

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -11,21 +11,21 @@ in {
   config = mkIf cfg.enable {
     environment.pathsToLink = [ "/libexec" ];
     services.xserver = {
+      enable = true;
       displayManager = {
         defaultSession = "none+i3";
       };
-      enable = true;
-    };
 
-    windowManager.i3 = {
-      enable = true;
-      package = pkgs.i3-gaps;
-      extraPackages = with pkgs; [
-        dmenu #application launcher most people use
-        i3status # gives you the default i3 status bar
-        i3lock #default i3 screen locker
-        i3blocks #if you are planning on using i3blocks over i3status
-     ];
+      windowManager.i3 = {
+        enable = true;
+        package = pkgs.i3-gaps;
+        extraPackages = with pkgs; [
+          dmenu #application launcher most people use
+          i3status # gives you the default i3 status bar
+          i3lock #default i3 screen locker
+          i3blocks #if you are planning on using i3blocks over i3status
+        ];
+      };
     };
   };
 }
