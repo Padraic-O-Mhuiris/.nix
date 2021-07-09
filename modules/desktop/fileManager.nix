c9a036bafd194a92f85ed25aa00cb1ec35dcf310
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-07-09 18:58:01 +0100

    fix

--- modules/desktop/fileManager.nix
+++ modules/desktop/fileManager.nix
@@ -14,13 +14,13 @@ in {
 
     services.xserver.displayManager.sessionCommands = ''
       # launches udiskie on display start and mounts drives
-      udiskie -Ns &
+      udiskie -Ns --deta
     '';
 
     home.configFile."udiskie/config.yml".text = ''
       ignore_device:
         - device_file: /dev/loop0
-        ignore: true
+          ignore: true
     '';
   };
 }
