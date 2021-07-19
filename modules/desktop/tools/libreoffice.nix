3326ad3a45dd083cea99ac60c080dfddfda2eb3a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-07-19 22:05:30 +0100

    fix mistake

--- modules/desktop/tools/libreoffice.nix
+++ modules/desktop/tools/libreoffice.nix
@@ -5,7 +5,7 @@ with lib.my;
 
 let cfg = config.modules.desktop.tools.libreoffice;
 in {
-  options.modules.desktop.fileManager = { enable = mkBoolOpt false; };
+  options.modules.desktop.tools.libreoffice = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable { user.packages = with pkgs; [ libreoffice ]; };
 }
