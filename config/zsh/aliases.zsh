2e80064cffe4cd816809070b27571065c8bcbf4f
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-07-26 21:57:11 +0100

    rearrange aliases

--- config/zsh/aliases.zsh
+++ config/zsh/aliases.zsh
@@ -52,9 +52,9 @@ alias ssc='sudo systemctl'
 
 if command -v exa >/dev/null; then
   alias exa="exa --group-directories-first --git";
-  alias l="exa -1";
-  alias ll="exa -lg";
-  alias la="LC_COLLATE=C exa -la";
+  alias la="exa -1";
+  alias l="exa -lg";
+  alias ll="LC_COLLATE=C exa -la";
 fi
 
 autoload -U zmv
