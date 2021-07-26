564a9d1f1dacae3fb6c0b428891d22de8a44538e
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-07-26 21:26:17 +0100

    readded option config for zsh

--- modules/shell/zsh.nix
+++ modules/shell/zsh.nix
@@ -6,7 +6,23 @@ let
   cfg = config.modules.shell.zsh;
   configDir = config.dotfiles.configDir;
 in {
-  options.modules.shell.zsh = with types; { enable = mkBoolOpt false; };
+  options.modules.shell.zsh = with types; {
+    enable = mkBoolOpt false;
+
+    aliases = mkOpt (attrsOf (either str path)) { };
+
+    rcInit = mkOpt' lines "" ''
+      Zsh lines to be written to $XDG_CONFIG_HOME/zsh/extra.zshrc and sourced by
+      $XDG_CONFIG_HOME/zsh/.zshrc
+    '';
+    envInit = mkOpt' lines "" ''
+      Zsh lines to be written to $XDG_CONFIG_HOME/zsh/extra.zshenv and sourced
+      by $XDG_CONFIG_HOME/zsh/.zshenv
+    '';
+
+    rcFiles = mkOpt (listOf (either str path)) [ ];
+    envFiles = mkOpt (listOf (either str path)) [ ];
+  };
 
   config = mkIf cfg.enable {
     users.defaultUserShell = pkgs.zsh;
