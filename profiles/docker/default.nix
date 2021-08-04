e759a49df3e5598994c006b2b14fb1d4877d15cf
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-04 22:34:28 +0100

    remove old profiles

--- profiles/docker/default.nix
+++
@@ -1,7 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  virtualisation.docker.enable = true;
-  users.users.padraic.extraGroups = [ "docker" ];
-  environment.systemPackages = with pkgs; [ docker docker-compose ];
-}
--- profiles/hledger/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  environment = {
-    systemPackages = with pkgs; [ ledger hledger hledger-ui hledger-web ];
-    variables = { LEDGER_FILE = "/home/padraic/.finance/finance.journal"; };
-  };
-}
