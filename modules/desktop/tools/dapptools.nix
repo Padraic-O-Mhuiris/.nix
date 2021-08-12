6a7cdaf44337ea2fc9d51ce19203e9836ec91d6c
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-13 00:02:05 +0100

    cleaner dapptools solution

--- modules/desktop/tools/dapptools.nix
+++ modules/desktop/tools/dapptools.nix
@@ -6,12 +6,7 @@ with lib.my;
 let
   cfg = config.modules.desktop.tools.dapptools;
 
-  dapptools = import (pkgs.fetchFromGitHub {
-    repo = "dapptools";
-    rev = "b018508967657800e7e7c1d07e6e454c5e284feb";
-    sha256 = "0jhlsm79vkq800ckx5ri9x7ybng0kf11s5rs32mlh9hnvlyxknzy";
-    owner = "dapphub";
-  }) { };
+  dapptools = import (inputs.dapptools) { };
 
 in {
   options.modules.desktop.tools.dapptools = { enable = mkBoolOpt false; };
