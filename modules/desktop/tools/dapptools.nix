1f5459f011dc396bdb60541b8bcefd170d609ee3
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-12 20:47:18 +0100

    chancing something

--- modules/desktop/tools/dapptools.nix
+++ modules/desktop/tools/dapptools.nix
@@ -5,13 +5,20 @@ with lib.my;
 
 let
   cfg = config.modules.desktop.tools.dapptools;
-  dapptools = inputs.dapptools;
+
+  dapptools = import (builtins.fetchGit rec {
+    name = "dapptools-${rev}";
+    url = "https://github.com/dapphub/dapptools";
+    rev = "3e62bd54dfbc23b76d9234f94a7387504b360d5d";
+    ref = "dapp/0.25.0";
+  }) { };
+
 in {
   options.modules.desktop.tools.dapptools = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
 
-    user.packages = with pkgs; [ hevm solc z3 cvc4 ];
+    user.packages = with pkgs; [ dapptools.hevm solc z3 cvc4 ];
 
   };
 }
