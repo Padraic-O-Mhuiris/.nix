60b17616928107c1fcc9ac5af4b1c222758e5801
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-14 22:59:26 +0100

    removed other networking

--- scripts/minimal-nix-flake-configuration/configuration.nix
+++ scripts/minimal-nix-flake-configuration/configuration.nix
@@ -40,14 +40,8 @@
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
 
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  # Per-interface useDHCP will be mandatory in the future, so this generated config
-  # replicates the default behaviour.
-  networking.useDHCP = false;
-  networking.interfaces.enp5s0.useDHCP = true;
-  networking.interfaces.wlp4s0.useDHCP = true;
-
   networking.networkmanager.enable = true;
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
