e84c7b31a9d6e8d1721217e3cc7972a579c5e5c2
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-17 16:23:48 +0100

    enable picom correctly

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -22,28 +22,30 @@ in {
       })
     ];
 
-    services.xserver = {
+    services = {
       picom.enable = true;
-      enable = true;
-      dpi = cfg.dpi;
-      displayManager = {
-        defaultSession = "none+i3";
-        autoLogin = {
-          enable = true;
-          user = config.user.name;
-        };
-      };
-
-      windowManager.i3 = {
+      xserver = {
         enable = true;
-        package = pkgs.i3-gaps;
-        configFile = ../../config/i3/config;
-        extraPackages = with pkgs; [
-          dmenu # application launcher most people use
-          i3status # gives you the default i3 status bar
-          i3lock # default i3 screen locker
-          i3blocks # if you are planning on using i3blocks over i3status
-        ];
+        dpi = cfg.dpi;
+        displayManager = {
+          defaultSession = "none+i3";
+          autoLogin = {
+            enable = true;
+            user = config.user.name;
+          };
+        };
+
+        windowManager.i3 = {
+          enable = true;
+          package = pkgs.i3-gaps;
+          configFile = ../../config/i3/config;
+          extraPackages = with pkgs; [
+            dmenu # application launcher most people use
+            i3status # gives you the default i3 status bar
+            i3lock # default i3 screen locker
+            i3blocks # if you are planning on using i3blocks over i3status
+          ];
+        };
       };
     };
 
