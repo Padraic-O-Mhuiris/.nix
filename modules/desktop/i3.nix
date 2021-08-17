36ddd6aa88d1808da97d6c936d6ad47dbc389f6c
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-17 16:19:10 +0100

    added dunst service

--- modules/desktop/i3.nix
+++ modules/desktop/i3.nix
@@ -45,5 +45,15 @@ in {
         ];
       };
     };
+
+    systemd.user.services."dunst" = {
+      enable = true;
+      description = "";
+      wantedBy = [ "default.target" ];
+      serviceConfig.Restart = "always";
+      serviceConfig.RestartSec = 2;
+      serviceConfig.ExecStart = "${pkgs.dunst}/bin/dunst";
+    };
+
   };
 }
