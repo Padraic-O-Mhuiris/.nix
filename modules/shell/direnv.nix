b96f265a0a17d8b5b8802643955bf490998801f5
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-19 21:44:04 +0100

    Revert "home-manager version"
    This reverts commit bc3995e1e273dfb3aada3a7965fc7e2da2beba69.

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -7,9 +7,25 @@ in {
   options.modules.shell.direnv = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    programs.direnv.enable = true;
-    programs.direnv.nix-direnv.enable = true;
-    # optional for nix flakes support
-    programs.direnv.nix-direnv.enableFlakes = true;
+
+    nix.extraOptions = ''
+      keep-outputs = true
+      keep-derivations = true
+    '';
+    environment.pathsToLink = [ "/share/nix-direnv" ];
+
+    user.packages = with pkgs; [ direnv nix-direnv ];
+
+    modules.shell.zsh.rcInit = ''eval "$(direnv hook zsh)"'';
+
+    pkgs.overlays = [
+      (self: super: {
+        nix-direnv = super.nix-direnv.override { enableFlakes = true; };
+      })
+    ];
+
+    home.file.".direnvrc".text = ''
+      source /run/current-system/sw/share/nix-direnv/direnvrc
+    '';
   };
 }
