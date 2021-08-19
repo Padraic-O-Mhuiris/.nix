1a059efd2f365ae81ebbbb56fa660596381d94a8
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-19 21:24:56 +0100

    reenable flake in nix-direnv

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -18,8 +18,11 @@ in {
 
     modules.shell.zsh.rcInit = ''eval "$(direnv hook zsh)"'';
 
-    nixpkgs.overlays =
-      [ (self: super: { nix-direnv = super.nix-direnv.override { }; }) ];
+    nixpkgs.overlays = [
+      (self: super: {
+        nix-direnv = super.nix-direnv.override { enableFlakes = true; };
+      })
+    ];
 
     home.file.".direnvrc".text = ''
       source /run/current-system/sw/share/nix-direnv/direnvrc
