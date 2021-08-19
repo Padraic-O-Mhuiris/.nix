632fd41483040a52d50dbbb202eb8af481f0c384
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-19 15:17:32 +0100

    use xst over st

--- modules/desktop/term/st.nix
+++ modules/desktop/term/st.nix
@@ -18,24 +18,36 @@ in {
     '';
 
     user.packages = with pkgs; [
-      (st.overrideAttrs (oldAttrs: rec {
-        src = fetchFromGitHub {
-          owner = "LukeSmithxyz";
-          repo = "st";
-          rev = "8ab3d03681479263a11b05f7f1b53157f61e8c3b";
-          sha256 = "1brwnyi1hr56840cdx0qw2y19hpr0haw4la9n0rqdn0r2chl8vag";
-        };
-        # Make sure you include whatever dependencies the fork needs to build properly!
-        buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
-      }))
+      xst # st + nice-to-have extensions
       (makeDesktopItem {
-        name = "st";
+        name = "xst";
         desktopName = "Suckless Terminal";
         genericName = "Default terminal";
         icon = "utilities-terminal";
-        exec = "${st}/bin/st";
+        exec = "${xst}/bin/xst";
         categories = "Development;System;Utility";
       })
     ];
+
+    # user.packages = with pkgs; [
+    #   (st.overrideAttrs (oldAttrs: rec {
+    #     src = fetchFromGitHub {
+    #       owner = "LukeSmithxyz";
+    #       repo = "st";
+    #       rev = "8ab3d03681479263a11b05f7f1b53157f61e8c3b";
+    #       sha256 = "1brwnyi1hr56840cdx0qw2y19hpr0haw4la9n0rqdn0r2chl8vag";
+    #     };
+    #     # Make sure you include whatever dependencies the fork needs to build properly!
+    #     buildInputs = oldAttrs.buildInputs ++ [ harfbuzz ];
+    #   }))
+    #   (makeDesktopItem {
+    #     name = "st";
+    #     desktopName = "Suckless Terminal";
+    #     genericName = "Default terminal";
+    #     icon = "utilities-terminal";
+    #     exec = "${st}/bin/st";
+    #     categories = "Development;System;Utility";
+    #   })
+    # ];
   };
 }
