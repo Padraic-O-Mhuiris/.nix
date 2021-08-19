9e50f222e2ffd52c75d71985de0d83a076eb1e11
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-19 21:44:38 +0100

    removed .direnvrc

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -18,14 +18,11 @@ in {
 
     modules.shell.zsh.rcInit = ''eval "$(direnv hook zsh)"'';
 
-    pkgs.overlays = [
+    nixpkgs.overlays = [
       (self: super: {
         nix-direnv = super.nix-direnv.override { enableFlakes = true; };
       })
     ];
 
-    home.file.".direnvrc".text = ''
-      source /run/current-system/sw/share/nix-direnv/direnvrc
-    '';
   };
 }
