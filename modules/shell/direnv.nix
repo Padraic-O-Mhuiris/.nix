567fd95d37a291658cefa68beb51881e890eb9a2
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-20 15:01:27 +0100

    reverting to semi-working version

--- modules/shell/direnv.nix
+++ modules/shell/direnv.nix
@@ -7,26 +7,20 @@ in {
   options.modules.shell.direnv = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [
-      (self: super: {
-        nix-direnv = super.nix-direnv.override {
-          enableFlakes = true;
-          nixUnstable = inputs.nixpkgs-unstable;
-        };
-      })
-    ];
-
     nix.extraOptions = ''
       keep-outputs = true
       keep-derivations = true
     '';
     environment.pathsToLink = [ "/share/nix-direnv" ];
 
-    user.packages = with pkgs; [ direnv unstable.nix-direnv ];
+    user.packages = with pkgs; [
+      direnv
+      (unstable.nix-direnv.override { enableFlakes = true; })
+    ];
 
     modules.shell.zsh.rcInit = ''eval "$(direnv hook zsh)"'';
 
-    home.file.".direnvrc".text = ''
+    home.configFile."direnv/direnvrc".text = ''
       source ${pkgs.unstable.nix-direnv}/share/nix-direnv/direnvrc
     '';
   };
