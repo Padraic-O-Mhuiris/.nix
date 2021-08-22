b5f645284c9b5a018ee475c6bb05d4083a7b5d6a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-22 17:15:14 +0100

    fixing steam

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -8,5 +8,9 @@ let cfg = config.modules.desktop.apps.steam;
 in {
   options.modules.desktop.apps.steam = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { programs.steam.enable = true; };
+  config = mkIf cfg.enable {
+
+    user.packages = with pkgs;
+      [ (pkgs.steam.override { extraLibraries = pkgs: [ pkgs.pipewire ]; }) ];
+  };
 }
