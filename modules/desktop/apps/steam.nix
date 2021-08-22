c7fdeeb003a2bbd8bcc447dba1d008e97357707a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-08-22 17:19:54 +0100

    added pipewire as individual package

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -8,5 +8,8 @@ let cfg = config.modules.desktop.apps.steam;
 in {
   options.modules.desktop.apps.steam = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { programs.steam.enable = true; };
+  config = mkIf cfg.enable {
+    programs.steam.enable = true;
+    user.packages = with pkgs; [ pipewire ];
+  };
 }
