cf9b9864f9134039384632bad22e9aff2504cf6f
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-10 21:42:46 +0100

    remove pass tomb references

--- modules/shell/pass.nix
+++ modules/shell/pass.nix
@@ -13,23 +13,24 @@ in {
     env = {
       PASSWORD_STORE_KEY = gnupgCfg.gpgPublicKey;
       PASSWORD_STORE_DIR = "$HOME/.secrets";
-      PASSWORD_STORE_TOMB_FILE = "$HOME/.secrets/${gnupgCfg.gpgPublicKey}.tomb";
-      PASSWORD_STORE_TOMB_KEY =
-        "/run/media/padraic/SHOVEL/${gnupgCfg.gpgPublicKey}.shovel.tomb";
+      #PASSWORD_STORE_TOMB_FILE = "$HOME/.secrets/${gnupgCfg.gpgPublicKey}.tomb";
+      #PASSWORD_STORE_TOMB_KEY =
+      #  "/run/media/padraic/SHOVEL/${gnupgCfg.gpgPublicKey}.shovel.tomb";
     };
 
-    user.packages = with pkgs; [
-      tomb
-      (pass.withExtensions (ext:
-        with ext; [
-          pass-audit
-          pass-otp
-          pass-import
-          pass-genphrase
-          pass-update
-          pass-tomb
-        ]))
-    ];
+    user.packages = with pkgs;
+      [
+        #tomb
+        (pass.withExtensions (ext:
+          with ext; [
+            pass-audit
+            pass-otp
+            pass-import
+            pass-genphrase
+            pass-update
+            #pass-tomb
+          ]))
+      ];
 
   };
 }
