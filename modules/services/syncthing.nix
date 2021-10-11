562b21da4f2dcf5f0acfbe5da18339704f06f833
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-11 15:11:07 +0100

    fixup

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -7,7 +7,9 @@ in {
   options.modules.services.syncthing = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    services.syncthing = { enable = true; };
-    dataDir = "/home/${config.user.name}/shared";
+    services.syncthing = {
+      enable = true;
+      dataDir = "/home/${config.user.name}/shared";
+    };
   };
 }
