d38340dbe3e07ba6e79df2f82f21d8817a2a50d3
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-19 15:46:51 +0100

    added agenix secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,5 +1,10 @@
 { config, lib, pkgs, ... }:
 
-{
+with lib;
+with lib.my;
 
-}
+let
+  keys = getHostKeys ../hosts;
+  Hydrogen = keys.Hydrogen;
+  Oxygen = keys.Oxygen;
+in { "secret1.age".publicKeys = [ Oxygen Hydrogen ]; }
