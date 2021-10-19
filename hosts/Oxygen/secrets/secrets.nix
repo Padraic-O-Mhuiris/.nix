f464b465ca0eb52dfc32d563a8b23db7b05d44aa
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-19 19:23:02 +0100

    change

--- hosts/Oxygen/secrets/secrets.nix
+++ hosts/Oxygen/secrets/secrets.nix
@@ -1,2 +1,4 @@
-let key = (builtins.readFile ../key.pub);
+let
+  key =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHXnABspqcYysmEtN8zKAjrUyxKy5RXm740h3csJWqy Oxygen - padraic-o-mhuiris@protonmail.com";
 in { "ngrok-config.age".publicKeys = [ key ]; }
