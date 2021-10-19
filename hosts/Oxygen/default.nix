02ea1b9b91a1eb8e153d1e2ff8b30cb430e05aee
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-19 18:55:15 +0100

    uhuh

--- hosts/Oxygen/default.nix
+++ hosts/Oxygen/default.nix
@@ -3,7 +3,7 @@
 {
   nix = { buildCores = 16; };
 
-  imports = [ ./hardware-configuration.nix ../home.nix ./modules/ngrok ];
+  imports = [ ./hardware-configuration.nix ../home.nix ./modules/ngrok.nix ];
 
   modules = {
     desktop = {
