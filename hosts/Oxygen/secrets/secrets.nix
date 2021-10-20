4050abdca98f15fde27f0de9f4f8ac1c100f05fd
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-20 21:04:12 +0100

    removed all secrets

--- hosts/Oxygen/secrets/secrets.nix
+++ hosts/Oxygen/secrets/secrets.nix
@@ -1,4 +1,4 @@
 let
   key =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFHXnABspqcYysmEtN8zKAjrUyxKy5RXm740h3csJWqy Oxygen - padraic-o-mhuiris@protonmail.com";
-in { "ngrok-config.age".publicKeys = [ key ]; }
+in { }
