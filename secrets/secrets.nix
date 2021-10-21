3c5ec75a17bb3b752501c8ec98108f59d43d2a2f
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-21 23:07:07 +0100

    update

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,4 +1,6 @@
 let
   Oxygen =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWMxDpxfrlXhAyln0+MKZs7q3i1VimlHhGgUxVVaeYY Oxygen";
+  Hydrogen =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen";
 in { "ngrokConfig.age" = { publicKeys = [ Oxygen ]; }; }
