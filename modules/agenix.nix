3b85e00b71c9f79725e85a7d27d47d7ee8aae319
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-22 00:24:10 +0100

    eckjb

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -12,14 +12,29 @@ in {
   environment.systemPackages = [ agenix.defaultPackage.x86_64-linux ];
 
   age = {
-    secrets = {
-      ngrokConfig = {
-        file = ../secrets/ngrokConfig.age;
-        mode = "0400";
-        owner = "ngrok";
-        group = "ngrok";
-      };
-    };
+    secrets = let
+      hostInPublicKeys = host: keyList:
+        lists.any (x: x == host)
+        (lists.map (s: lists.last (list.splitString " " s)) keyList);
+
+      filterByHost = host: secrets:
+        (filterAttrs
+          (n: v: hostInPublicKeys config.networking.hostName v.publicKeys)
+          secrets);
+
+      buildAgeSecretsByHost = fn: host: secrets:
+        mapAttrs' fn (filterByHost host (secrets));
+
+    in if pathExists secretsFile then
+      buildAgeSecretsByHost (n: v:
+        nameValuePair (removeSuffix ".age" n) {
+          file = "${secretsDir}/${n}";
+          mode = v.mode;
+          owner = v.owner;
+          group = v.group;
+        }) (config.networking.hostName) (import secretsFile)
+    else
+      { };
     sshKeyPaths = [ "${config.user.home}/.ssh/id_ed25519" ];
   };
 }
