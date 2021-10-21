de36be0937024e5058fe4e3f66e8cdb048328c36
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-21 23:18:59 +0100

    updat3e

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,9 @@
 let
+  padraic = ''
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFlro/QUDlDpaA1AQxdWIqBg9HSFJf9Cb7CPdsh0JN7 padraic-o-mhuiris@protonmail.com
+  '';
   Oxygen =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWMxDpxfrlXhAyln0+MKZs7q3i1VimlHhGgUxVVaeYY Oxygen";
   Hydrogen =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen";
-in { "ngrokConfig.age" = { publicKeys = [ Oxygen ]; }; }
+in { "ngrokConfig.age" = { publicKeys = [ padraic Oxygen ]; }; }
