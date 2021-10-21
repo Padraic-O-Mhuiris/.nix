dced99eb9c4d9b3d2bdc872c7868810533b13947
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-21 22:59:46 +0100

    simplify secrets

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -5,23 +5,21 @@ with lib;
 with lib.my;
 let
   inherit (inputs) agenix;
-  secretsDir = "../secrets";
+  secretsDir = ../secrets;
   secretsFile = "${secretsDir}/secrets.nix";
 in {
   imports = [ agenix.nixosModules.age ];
   environment.systemPackages = [ agenix.defaultPackage.x86_64-linux ];
 
   age = {
-    secrets = if pathExists (./. + secretsFile) then
-      mapAttrs' (n: v:
-        nameValuePair (removeSuffix ".age" n) {
-          file = "${secretsDir}/${n}";
-          owner = v.owner;
-          group = v.group;
-          mode = mkDefault "0400";
-        }) (import secretsFile)
-    else
-      { };
+    secrets = {
+      ngrokConfig = {
+        file = ../secrets/ngrokConfig.age;
+        mode = "0400";
+        owner = "ngrok";
+        group = "ngrok";
+      };
+    };
     sshKeyPaths = [ "${config.user.home}/.ssh/id_ed25519" ];
   };
 }
