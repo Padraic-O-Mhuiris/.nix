6d37a7ccb397a06586c9dd7224c4bad43b01f667
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-22 14:03:26 +0100

    update latest

--- hosts/Hydrogen/secrets/secrets.nix
+++ hosts/Hydrogen/secrets/secrets.nix
@@ -3,4 +3,9 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFlro/QUDlDpaA1AQxdWIqBg9HSFJf9Cb7CPdsh0JN7 padraic-o-mhuiris@protonmail.com";
   Hydrogen =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen";
-in { }
+in {
+  "sshConfig.age" = {
+    publicKeys = [ padraic Hydrogen ];
+    owner = "padraic";
+  };
+}
