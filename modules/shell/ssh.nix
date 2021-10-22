773e1f44927463d3472cce0ad6b8f427cececd8a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-22 12:31:36 +0100

    ccc

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -16,58 +16,75 @@ in {
   options.modules.shell.ssh = {
     enable = mkBoolOpt false;
     enableRemoteAccess = mkBoolOpt false;
+    configFile = mkOpt (either path null);
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf cfg.enable (mkMerge [
+    {
+      user.packages = with pkgs; [ is_local_conn ];
+      services.openssh = {
+        enable = true;
+        passwordAuthentication = false;
+        permitRootLogin = "no";
+        hostKeys = [{
+          type = "ed25519";
+          path = "/etc/ssh/ssh_host_ed25519_key";
+          rounds = 100;
+          comment = "${config.networking.hostName}";
+        }];
+      };
+      user.openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWMxDpxfrlXhAyln0+MKZs7q3i1VimlHhGgUxVVaeYY Oxygen"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen"
+      ];
+    }
 
-    user.packages = with pkgs; [ is_local_conn ];
+    (mkIf (cfg.configFile == null) {
 
-    services.openssh = {
-      enable = true;
-      passwordAuthentication = false;
-      permitRootLogin = "no";
-      hostKeys = [{
-        type = "ed25519";
-        path = "/etc/ssh/ssh_host_ed25519_key";
-        rounds = 100;
-        comment = "${config.networking.hostName}";
-      }];
-    };
+      systemd.services."ssh_config_overwrite" = {
+        description = "Overwrite local ssh config";
+        serviceConfig = {
+          Type = "oneshot";
+          User = config.user.name;
+          Group = config.user.group;
+          RemainAfterExit = "yes";
+          StandardOutput = "journal";
+          ExecStart = ''
+            /bin/bash -c "echo Oneshot service - start && sleep 60 && echo Oneshot service - end"'';
+        };
+      };
 
-    user.openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWMxDpxfrlXhAyln0+MKZs7q3i1VimlHhGgUxVVaeYY Oxygen"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen"
-    ];
+    })
 
-    # modules.services.ngrok = {
-    #   enable = cfg.enableRemoteAccess;
-    #   configFile = config.age.secrets.ngrokConfig.path;
-    # };
-    # home.file.".ssh/config".text = ''
-    #   Host HydrogenLocal
-    #     Hostname 192.168.0.26
+  ]);
+  # modules.services.ngrok = {
+  #   enable = cfg.enableRemoteAccess;
+  #   configFile = config.age.secrets.ngrokConfig.path;
+  # };
+  # home.file.".ssh/config".text = ''
+  #   Host HydrogenLocal
+  #     Hostname 192.168.0.26
 
-    #   Host OxygenLocal
-    #     Hostname 192.168.0.158
+  #   Host OxygenLocal
+  #     Hostname 192.168.0.158
 
-    #   Match host Nitrogen exec is_local_conn
-    #     HostName 192.168.0.55
-    #     Port 22175
+  #   Match host Nitrogen exec is_local_conn
+  #     HostName 192.168.0.55
+  #     Port 22175
 
-    #   Match host Nitrogen
-    #     Hostname 1.tcp.eu.ngrok.io
-    #     Port 26096
+  #   Match host Nitrogen
+  #     Hostname 1.tcp.eu.ngrok.io
+  #     Port 26096
 
-    #   Host *
-    #     User ${config.user.name}
-    #     IdentityFile ~/.ssh/id_rsa.pub
-    # '';
+  #   Host *
+  #     User ${config.user.name}
+  #     IdentityFile ~/.ssh/id_rsa.pub
+  # '';
 
-    # systemd.services.ngrok = {
-    #   wantedBy = [ "multi-user.target" ];
-    #   after = [ "network.target" ];
-    #   description = "ngrok ssh tunnel";
-    #   serviceConfig = { ExecStart = "${pkgs.ngrok}/bin/ngrok tcp 22"; };
-    # };
-  };
+  # systemd.services.ngrok = {
+  #   wantedBy = [ "multi-user.target" ];
+  #   after = [ "network.target" ];
+  #   description = "ngrok ssh tunnel";
+  #   serviceConfig = { ExecStart = "${pkgs.ngrok}/bin/ngrok tcp 22"; };
+  # };
 }
