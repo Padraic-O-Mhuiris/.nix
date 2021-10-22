81135f9802709803aaf68d0a04dae32b2ede1ffe
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-22 13:27:42 +0100

    elkcn

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -12,6 +12,7 @@ let
     exit 1
   '';
 
+  overwriteDir = "/var/lib/overwrites";
 in {
   options.modules.shell.ssh = with types; {
     enable = mkBoolOpt false;
@@ -33,16 +34,21 @@ in {
         }];
       };
       user.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWMxDpxfrlXhAyln0+MKZs7q3i1VimlHhGgUxVVaeYY Oxygen"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFlro/QUDlDpaA1AQxdWIqBg9HSFJf9Cb7CPdsh0JN7 padraic-o-mhuiris@protonmail.com"
       ];
     }
 
     (mkIf (cfg.sshConfigFile != null) {
 
+      systemd.tmpfiles.rules = [ "d ${overwriteDir} 0755 root root - -" ];
+
       systemd.services."ssh_config_overwrite" = {
         description = "Overwrite local ssh config";
         wantedBy = [ "multi-user.target" ];
+        preStart = ''
+          [ -f ${cfg.sshConfigFile} ] && ${pkgs.coreutils}/bin/install -m 0400 ${cfg.sshConfigFile} ${overwriteDir}/ssh_config
+        '';
+
         serviceConfig = {
           Type = "simple";
           User = config.user.name;
@@ -50,41 +56,11 @@ in {
           RemainAfterExit = "yes";
           StandardOutput = "journal";
           ExecStart = ''
-            ${pkgs.bash}/bin/bash -c "[ -f ${cfg.sshConfigFile} ] && cp ${cfg.sshConfigFile} /home/${config.user.name}/sss";
+            ${pkgs.bash}/bin/bash -c "cp ${overwriteDir}/ssh_config /home/${config.user.name}/.ssh/config";
           '';
           Restart = "always";
         };
       };
     })
   ]);
-  # modules.services.ngrok = {
-  #   enable = cfg.enableRemoteAccess;
-  #   configFile = config.age.secrets.ngrokConfig.path;
-  # };
-  # home.file.".ssh/config".text = ''
-  #   Host HydrogenLocal
-  #     Hostname 192.168.0.26
-
-  #   Host OxygenLocal
-  #     Hostname 192.168.0.158
-
-  #   Match host Nitrogen exec is_local_conn
-  #     HostName 192.168.0.55
-  #     Port 22175
-
-  #   Match host Nitrogen
-  #     Hostname 1.tcp.eu.ngrok.io
-  #     Port 26096
-
-  #   Host *
-  #     User ${config.user.name}
-  #     IdentityFile ~/.ssh/id_rsa.pub
-  # '';
-
-  # systemd.services.ngrok = {
-  #   wantedBy = [ "multi-user.target" ];
-  #   after = [ "network.target" ];
-  #   description = "ngrok ssh tunnel";
-  #   serviceConfig = { ExecStart = "${pkgs.ngrok}/bin/ngrok tcp 22"; };
-  # };
 }
