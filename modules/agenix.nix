bd6b919921b804e347f9886dec2d2427e5681531
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-22 12:13:51 +0100

    changed files

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -4,31 +4,21 @@ with builtins;
 with lib;
 let
   inherit (inputs) agenix;
-  secretsDir = ../secrets;
+  secretsDir = "${toString ../hosts}/${config.networking.hostName}/secrets";
   secretsFile = "${secretsDir}/secrets.nix";
-
-  hostInPublicKeys = host: keyList:
-    lists.any (x: x == host)
-    (lists.map (s: lists.last (splitString " " s)) keyList);
-
-  buildAgeSecretsForHost = host: secrets:
-    mapAttrs (n: v:
-      if (hostInPublicKeys config.networking.hostName v.publicKeys) then {
-        file = "${secretsDir}/${n}";
-      } else
-        { }) secrets;
-
 in {
   imports = [ agenix.nixosModules.age ];
   environment.systemPackages = [ agenix.defaultPackage.x86_64-linux ];
 
   age = {
     secrets = if pathExists secretsFile then
-      buildAgeSecretsForHost config.network.hostName (import secretsFile)
+      mapAttrs' (n: _:
+        nameValuePair (removeSuffix ".age" n) {
+          file = "${secretsDir}/${n}";
+          owner = mkDefault config.user.name;
+        }) (import secretsFile)
     else
       { };
-    #secrets = { ngrokConfig = { file = ../secrets/ngrokConfig.age; }; };
-
     sshKeyPaths = [ "${config.user.home}/.ssh/id_ed25519" ];
   };
 }
