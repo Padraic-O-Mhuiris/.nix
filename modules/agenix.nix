9dc039163c8f174d7535e5ac13569faf4b29e517
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-22 11:21:45 +0100

    cjhewbcj

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -2,7 +2,6 @@
 
 with builtins;
 with lib;
-with lib.my;
 let
   inherit (inputs) agenix;
   secretsDir = ../secrets;
@@ -11,29 +10,29 @@ in {
   imports = [ agenix.nixosModules.age ];
   environment.systemPackages = [ agenix.defaultPackage.x86_64-linux ];
 
-  # age = {
-  #   secrets = let
-  #     hostInPublicKeys = host: keyList:
-  #       lists.any (x: x == host)
-  #       (lists.map (s: lists.last (splitString " " s)) keyList);
-
-  #     filterByHost = host: secrets:
-  #       (filterAttrs (n: v: hostInPublicKeys host v.publicKeys) secrets);
-
-  #     buildAgeSecretsByHost = fn: host: secrets:
-  #       mapAttrs' fn (filterByHost host secrets);
-
-  #   in if pathExists secretsFile then
-  #     buildAgeSecretsByHost (n: v:
-  #       nameValuePair (removeSuffix ".age" n) {
-  #         file = "${secretsDir}/${n}";
-  #         mode = "0400";
-  #       }) (config.networking.hostName) (import secretsFile)
-  #   else
-  #     { };
-
   age = {
-    secrets = { ngrokConfig = { file = ../secrets/ngrokConfig.age; }; };
+    secrets = let
+      hostInPublicKeys = host: keyList:
+        lists.any (x: x == host)
+        (lists.map (s: lists.last (splitString " " s)) keyList);
+
+      filterByHost = host: secrets:
+        (filterAttrs (n: v: hostInPublicKeys host v.publicKeys) secrets);
+
+      buildAgeSecretsByHost = fn: host: secrets:
+        mapAttrs' fn (filterByHost host secrets);
+
+      fn = (n: v:
+        nameValuePair (removeSuffix ".age" n) { file = "${secretsDir}/${n}"; });
+
+      hostName = config.networking.hostName;
+    in if pathExists secretsFile then
+      buildAgeSecretsByHost fn "Oxygen" (import secretsFile)
+    else
+      { };
+
+    #secrets = { ngrokConfig = { file = ../secrets/ngrokConfig.age; }; };
+
     sshKeyPaths = [ "${config.user.home}/.ssh/id_ed25519" ];
   };
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,4 +5,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWMxDpxfrlXhAyln0+MKZs7q3i1VimlHhGgUxVVaeYY Oxygen";
   Hydrogen =
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIHx6hOIV9jksyymGefvsRoAwGfAPIur92VFFGUUDwj8 Hydrogen";
-in { "ngrokConfig.age" = { publicKeys = [ padraic Oxygen ]; }; }
+in {
+  "ngrokConfig.age" = { publicKeys = [ padraic Oxygen ]; };
+  "secret1.age" = { publicKeys = [ padraic Hydrogen ]; };
+}
