183a23f4c5cafe9ad78791c846bf26f7207544a7
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-24 18:55:36 +0100

    update

--- modules/desktop/apps/ledger.nix
+++ modules/desktop/apps/ledger.nix
@@ -7,6 +7,10 @@ let cfg = config.modules.desktop.apps.ledger;
 in {
   options.modules.desktop.apps.ledger = { enable = mkBoolOpt false; };
 
+  users.groups.plugdev = { };
+
+  users."${config.user.name}" = { extraGroups = [ "plugdev" ]; };
+
   config = mkIf cfg.enable {
     user.packages = with pkgs; [ ledger-live-desktop ledger-udev-rules ];
   };
