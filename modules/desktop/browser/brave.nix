8314a3524d285aecf234acbe46be1176d9e282f0
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-24 22:37:39 +0100

    cecw

--- modules/desktop/browser/brave.nix
+++ modules/desktop/browser/brave.nix
@@ -14,17 +14,15 @@ in {
         desktopName = "Personal Browser";
         genericName = "Opens personal Brave profile";
         icon = "brave";
-        exec = ''
-          ${brave}/bin/brave --profile-directory "$HOME/.config/BraveSoftware/BraveSoftware/Profile 1"'';
+        exec = ''${brave}/bin/brave --profile-directory="Default"'';
         categories = "Network";
       })
       (makeDesktopItem {
-        name = "brave-worl";
+        name = "brave-work";
         desktopName = "Work Browser";
         genericName = "Opens work Brave profile";
         icon = "brave";
-        exec = ''
-          ${brave}/bin/brave --profile-directory "$HOME/.config/BraveSoftware/BraveSoftware/Profile 2"'';
+        exec = ''${brave}/bin/brave --profile-directory="Profile 1"'';
         categories = "Network";
       })
     ];
