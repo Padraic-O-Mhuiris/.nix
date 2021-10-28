a69df2d5c2ac1351d88c38a259ab6a245ed50118
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-10-28 12:49:14 +0100

    change eth rpc

--- modules/shell/dapptools.nix
+++ modules/shell/dapptools.nix
@@ -13,9 +13,9 @@ in {
 
   config = mkIf cfg.enable {
 
-    home.configFile."sethrc".text = ''
+    home.configFile."seth/sethrc".text = ''
       export ETH_FROM=0x0000000000000000000000000000000000000000
-      export ETH_RPC_URL=https://mainnet.infura.io/v3/7b2295eb2ca8443fba441bfd462cd93a
+      export ETH_RPC_URL=https://localhost:9000
     '';
 
     user.packages = with pkgs;
