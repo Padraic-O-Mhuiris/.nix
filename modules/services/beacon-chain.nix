6f236e1d2d861f09a01d527c4ce4d5af09e79564
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-12-16 21:59:50 +0000

    kenclkjn

--- modules/services/beacon-chain.nix
+++ modules/services/beacon-chain.nix
@@ -5,7 +5,8 @@ with lib.my;
 
 let cfg = config.modules.services.beacon-chain;
 in {
-  options.modules.services.geth = { enable = mkBoolOpt false; };
+  options.modules.services.beacon-chain = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { user.packages = [ prysm-beacon-chain ]; };
+  config =
+    mkIf cfg.enable { user.packages = with pkgs; [ my.prysm-beacon-chain ]; };
 }
