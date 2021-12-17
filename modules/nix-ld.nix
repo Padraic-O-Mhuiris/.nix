bdee86f31f93da95541ffc65d45fd1d9324a3b4a
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-12-17 11:35:47 +0000

    kcej

--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -1,4 +1,11 @@
-{ config, inputs, lib, pkgs, ... }:
+{ config, inputs, lib, pkgs, stdenv, ... }:
 
 let inherit (inputs) nix-ld;
-in { imports = [ nix-ld.nixosModules.nix-ld ]; }
+in {
+  imports = [ nix-ld.nixosModules.nix-ld ];
+
+  environment.variables = {
+    NIX_LD_LIBRARY_PATH = lib.makeLibraryPath [ stdenv.cc.cc ];
+    NIX_LD = lib.fileContents "${stdenv.cc}/nix-support/dynamic-linker";
+  };
+}
