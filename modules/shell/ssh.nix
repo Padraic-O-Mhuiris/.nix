58c0555bdb02f62f7594adac89359c746654b236
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2021-12-18 17:47:45 +0000

    celmjb

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -18,42 +18,41 @@ in {
     sshConfigFile = mkPathOpt null;
   };
 
-  config = mkIf cfg.enable (mkMerge [
-    {
-      user.packages = with pkgs; [ is_local_conn ];
-      services.openssh = {
-        enable = true;
-        passwordAuthentication = false;
-        permitRootLogin = "no";
-        hostKeys = [{
-          type = "ed25519";
-          path = "/etc/ssh/ssh_host_ed25519_key";
-          rounds = 100;
-          comment = "${config.networking.hostName}";
-        }];
-      };
-      user.openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFlro/QUDlDpaA1AQxdWIqBg9HSFJf9Cb7CPdsh0JN7 padraic-o-mhuiris@protonmail.com"
-      ];
-    }
+  config = mkIf cfg.enable (mkMerge [{
+    user.packages = with pkgs; [ is_local_conn ];
+    services.openssh = {
+      enable = true;
+      passwordAuthentication = false;
+      permitRootLogin = "no";
+      hostKeys = [{
+        type = "ed25519";
+        path = "/etc/ssh/ssh_host_ed25519_key";
+        rounds = 100;
+        comment = "${config.networking.hostName}";
+      }];
+    };
+    user.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFlro/QUDlDpaA1AQxdWIqBg9HSFJf9Cb7CPdsh0JN7 padraic-o-mhuiris@protonmail.com"
+    ];
+  }
 
-    (mkIf (cfg.sshConfigFile != null) {
+  # (mkIf (cfg.sshConfigFile != null) {
 
-      systemd.services."ssh_config_overwrite" = {
-        description = "Overwrite local ssh config";
-        wantedBy = [ "multi-user.target" ];
+  #   systemd.services."ssh_config_overwrite" = {
+  #     description = "Overwrite local ssh config";
+  #     wantedBy = [ "multi-user.target" ];
 
-        serviceConfig = {
-          Type = "oneshot";
-          User = "${config.user.name}";
-          Group = "${config.user.group}";
-          RemainAfterExit = "yes";
-          StandardOutput = "journal";
-          ExecStart = ''
-            [ -f ${cfg.sshConfigFile} ] && ${pkgs.coreutils}/bin/install -o ${config.user.name} -g ${config.user.group} -m 0400 ${cfg.sshConfigFile} /home/${config.user.name}/ssh/config
-          '';
-        };
-      };
-    })
-  ]);
+  #     serviceConfig = {
+  #       Type = "oneshot";
+  #       User = "${config.user.name}";
+  #       Group = "${config.user.group}";
+  #       RemainAfterExit = "yes";
+  #       StandardOutput = "journal";
+  #       ExecStart = ''
+  #         [ -f ${cfg.sshConfigFile} ] && ${pkgs.coreutils}/bin/install -o ${config.user.name} -g ${config.user.group} -m 0400 ${cfg.sshConfigFile} /home/${config.user.name}/ssh/config
+  #       '';
+  #     };
+  #   };
+  # })
+    ]);
 }
