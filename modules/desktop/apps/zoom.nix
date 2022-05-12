2e491c361ba0a5e5091b57b4328c32c81749d88d
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-05-12 21:08:55 +0100

    update zoom

--- modules/desktop/apps/zoom.nix
+++ modules/desktop/apps/zoom.nix
@@ -7,5 +7,5 @@ let cfg = config.modules.desktop.apps.zoom;
 in {
   options.modules.desktop.apps.zoom = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { user.packages = with pkgs; [ unstable.zoom-us ]; };
+  config = mkIf cfg.enable { user.packages = with pkgs; [ zoom-us ]; };
 }
