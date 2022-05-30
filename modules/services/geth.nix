7f58f1b5117a86f0a44b9d55a64136aa536843b8
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-05-30 12:24:31 +0100

    revert

--- modules/services/geth.nix
+++ modules/services/geth.nix
@@ -20,7 +20,6 @@ in {
         metrics.enable = false;
         syncmode = "full";
         package = pkgs.unstable.go-ethereum.geth; # always use latest
-        extraArgs = [ "snapshot prune-state" ];
       };
     };
 
