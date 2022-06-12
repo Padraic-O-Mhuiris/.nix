44af1e2b176db755ca20aa21bc14ae1d2c7b30cc
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-06-12 16:31:38 +0100

    use python39Full

--- modules/editors/languages/python.nix
+++ modules/editors/languages/python.nix
@@ -6,12 +6,5 @@ let cfg = config.modules.editors.languages.python;
 in {
   options.modules.editors.languages.python = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable {
-    user.packages = with pkgs.python37Packages; [
-      python
-      pip
-      setuptools
-      virtualenv
-    ];
-  };
+  config = mkIf cfg.enable { user.packages = with pkgs; [ python39Full ]; };
 }
