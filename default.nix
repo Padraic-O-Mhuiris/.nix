2cd68b36e79396beea00e5b7f8ea08307b969d4b
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-06-12 15:40:34 +0100

    revert state version

--- default.nix
+++ default.nix
@@ -46,9 +46,8 @@ with lib.my; {
     registry = registryInputs // { dotfiles.flake = inputs.self; };
     autoOptimiseStore = true;
   };
-
   system.configurationRevision = with inputs; mkIf (self ? rev) self.rev;
-  system.stateVersion = "22.05";
+  system.stateVersion = "21.11";
 
   ## Some reasonable, global defaults
   # This is here to appease 'nix flake check' for generic hosts with no
