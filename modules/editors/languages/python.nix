2cce3220d8cbe957ce558c1c98652aa857237b1b
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-06-12 21:06:55 +0100

    updates

--- modules/editors/languages/python.nix
+++ modules/editors/languages/python.nix
@@ -6,5 +6,16 @@ let cfg = config.modules.editors.languages.python;
 in {
   options.modules.editors.languages.python = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { user.packages = with pkgs; [ python39Full ]; };
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [
+      python39Full
+      python-language-server
+      nodePackages.pyright
+      black
+      python39Packages.nose
+      python39Packages.isort
+      python39Packages.pyflakes
+      poetry
+    ];
+  };
 }
