22a23644b67dc0e27cbacda3bb31eed517a61304
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-07-22 15:09:25 +0100

    update

--- modules/editors/languages/javascript.nix
+++ modules/editors/languages/javascript.nix
@@ -8,7 +8,7 @@ in {
 
   config = mkIf cfg.enable {
     user.packages = with pkgs.unstable; [
-      nodejs-16_x
+      nodejs-14_x
       nodePackages.yalc
       nodePackages.typescript-language-server
       nodePackages.javascript-typescript-langserver
