4564001f0edabff2bdc4f91128f4291945e33061
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-08-13 13:39:07 +0100

    refactor zfs.nix

--- hosts/Hydrogen/zfs.nix
+++ hosts/Hydrogen/zfs.nix
@@ -3,30 +3,44 @@
 {
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "3f90d23a";
-  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  boot.loader.efi.canTouchEfiVariables = false;
-  boot.loader.generationsDir.copyKernels = true;
-  boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.copyKernels = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.zfsSupport = true;
-  boot.loader.grub.extraPrepareConfig = ''
-    mkdir -p /boot/efis
-    for i in  /boot/efis/*; do mount $i ; done
+  boot = {
+    kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+    loader = {
+      efi = {
+        efiSysMountPoint = "/boot/efi";
+        canTouchEfiVariables = false;
+      };
+      generationsDir.copyKernels = true;
+      grub = {
+        efiInstallAsRemovable = true;
+        enable = true;
+        version = 2;
+        copyKernels = true;
+        efiSupport = true;
+        zfsSupport = true;
+        font = "${
+            builtins.toPath pkgs.iosevka
+          }/share/fonts/truetype/iosevka-regular.ttf";
+        fontSize = 30;
+        gfxmodeEfi = "3840x2400";
+        gfxmodeBios = "3840x2400";
+        extraPrepareConfig = ''
+          mkdir -p /boot/efis
+          for i in  /boot/efis/*; do mount $i ; done
 
-    mkdir -p /boot/efi
-    mount /boot/efi
-  '';
-  boot.loader.grub.extraInstallCommands = ''
-    ESP_MIRROR=$(mktemp -d)
-    cp -r /boot/efi/EFI $ESP_MIRROR
-    for i in /boot/efis/*; do
-     cp -r $ESP_MIRROR/EFI $i
-    done
-    rm -rf $ESP_MIRROR
-  '';
-  boot.loader.grub.devices = [ "/dev/disk/by-path/pci-0000:02:00.0-nvme-1" ];
+          mkdir -p /boot/efi
+          mount /boot/efi
+        '';
+        extraInstallCommands = ''
+          ESP_MIRROR=$(mktemp -d)
+          cp -r /boot/efi/EFI $ESP_MIRROR
+          for i in /boot/efis/*; do
+           cp -r $ESP_MIRROR/EFI $i
+          done
+          rm -rf $ESP_MIRROR
+        '';
+        devices = [ "/dev/disk/by-path/pci-0000:02:00.0-nvme-1" ];
+      };
+    };
+  };
 }
