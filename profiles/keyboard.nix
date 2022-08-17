f0d487c034ba652acbedb47fc662f1c1123ee0c5
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-08-17 22:02:11 +0100

    update

--- profiles/keyboard.nix
+++ profiles/keyboard.nix
@@ -13,12 +13,12 @@
       {
         keys = [ 233 ];
         events = [ "key" ];
-        command = "/run/current-system/sw/bin/light -A 3";
+        command = "${pkgs.light}/bin/light -A 3";
       }
       {
         keys = [ 232 ];
         events = [ "key" ];
-        command = "/run/current-system/sw/bin/light -U 3";
+        command = "${pkgs.light}/bin/light -U 3";
       }
     ];
   };
--- profiles/networking.nix
+++ profiles/networking.nix
@@ -19,5 +19,8 @@
       rounds = 100;
       comment = "${config.networking.hostName}";
     }];
+    user.openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEFlro/QUDlDpaA1AQxdWIqBg9HSFJf9Cb7CPdsh0JN7 padraic-o-mhuiris@protonmail.com"
+    ];
   };
 }
