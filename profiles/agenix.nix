59a3a8423deda0e6b46bbcd7c1369d71dcad56cd
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-08-17 23:24:17 +0100

    added agenix module

--- profiles/agenix.nix
+++ profiles/agenix.nix
@@ -1,5 +1,29 @@
-{ config, lib, pkgs, agenix, ... }:
+# This file searches for a /<HOST>/secrets/secrets.nix file and automatically
+# adds the secret to a module config
 
-{
-  user.packages = [ agenix.defaultPackage.x86_64-linux ];
+{ config, lib, pkgs, inputs, ... }:
+
+with builtins;
+with lib;
+let
+  secretsDir = "${toString ../hosts}/${config.networking.hostName}/secrets";
+  secretsFile = "${secretsDir}/secrets.nix";
+in {
+  environment.systemPackages = [ inputs.agenix.defaultPackage.x86_64-linux ];
+
+  age = {
+    secrets = if builtins.pathExists secretsFile then
+      mapAttrs' (n: v:
+        nameValuePair (removeSuffix ".age" n) {
+          file = "${secretsDir}/${n}";
+          owner = v.owner or config.user.name;
+          group = v.group or config.user.group;
+          path = v.path;
+          symlink = false;
+        }) (import secretsFile)
+    else
+      { };
+    sshKeyPaths = [ "${config.user.home}/.ssh/id_ed25519" ];
+  };
+
 }
