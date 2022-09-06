4885ac4487de1e88173ec3422907cafe4444ff58
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-09-06 14:57:01 +0100

    update

--- hosts/Oxygen/default.nix
+++ hosts/Oxygen/default.nix
@@ -5,6 +5,16 @@
 
   imports = [ ./hardware-configuration.nix ];
 
+  time.timeZone = "Europe/Dublin";
+  i18n.defaultLocale = "en_IE.UTF-8";
+
+  location = {
+    latitude = 53.28;
+    longitude = -9.03;
+  };
+
+  system.stateVersion = "22.05";
+
   # boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.zfs.enableUnstable = true;
