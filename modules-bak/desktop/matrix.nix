c0c068322a0984f313daad5d94399cb3f8f06c4e
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-10-16 05:25:48 +0100

    update

--- modules-bak/desktop/matrix.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, inputs, ... }:
-
-with lib;
-with lib.my;
-let cfg = config.modules.desktop.matrix;
-in {
-  options.modules.desktop.matrix = { enable = mkBoolOpt false; };
-  config = mkIf cfg.enable { user.packages = with pkgs; [ element-desktop ]; };
-}
--- modules-bak/theme.nix
+++
@@ -1,5 +0,0 @@
-{ options, config, inputs, lib, pkgs, ... }:
-
-{
-  imports = [ inputs.base16.nixosModule ];
-}
