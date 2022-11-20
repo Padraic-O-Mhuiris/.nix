b08a6a1992a8300318a7f793f5f59a0b1039d63e
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-11-20 20:24:20 +0000

    update

--- hosts/Hydrogen/nvidia.nix
+++ hosts/Hydrogen/nvidia.nix
@@ -4,7 +4,12 @@
   hardware = {
     video.hidpi.enable = true;
     opengl.enable = true;
-    #nvidia.modesetting.enable = true;
+    nvidia.modesetting.enable = false;
+    nvidia.prime.offload.enable = true;
+    nvidia.prime.nvidiaBusId = "PCI:1:0:0";
+    nvidia.prime.intelBusId = "PCI:0:2:0";
+    nvidia.powerManagement.enable = true;
+    nvidia.powerManagement.finegrained = true;
   };
   user.packages = with pkgs; [ glxinfo nvtop ];
 }
