c7997ba4b714d3dc4ebd02de39d7724bbe5fcc3f
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-11-20 20:07:36 +0000

    use offload

--- hosts/Hydrogen/nvidia.nix
+++ hosts/Hydrogen/nvidia.nix
@@ -4,24 +4,7 @@
   hardware = {
     video.hidpi.enable = true;
     opengl.enable = true;
-    nvidia = {
-      powerManagement = {
-        enable = true;
-        finegrained = true;
-      };
-      modesetting.enable = true;
-      prime = {
-        sync.enable = true;
-        offload.enable = false;
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
-      };
-    };
+    nvidia.modesetting.enable = true;
   };
-  services.xserver = {
-    videoDrivers = [ "nvidia" ];
-    displayManager.setupCommands = "";
-  };
-
   user.packages = with pkgs; [ glxinfo nvtop ];
 }
