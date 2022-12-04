b67d364477ae3b9b8af52ee900a34b1e86f72853
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-12-04 20:44:06 +0000

    change to latest kernel package

--- modules/machine/default.nix
+++ modules/machine/default.nix
@@ -6,8 +6,7 @@ with lib.os;
 {
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
   options.os.machine = {
-    kernel = mkOpt types.attrs
-      pkgs.linuxPackages_6_0; # TODO fix to correct submodule type if possible
+    kernel = mkOpt types.attrs pkgs.master.linuxPackages_latest;
     cores = mkOpt types.int 4;
     gpu = mkOpt (type.enum [ "integrated" "nvidia" ]) "integrated";
   };
