8846fce7d3a2840e6594395a22e203e286301d58
padraic <patrick.morris.310@gmail.com>
2022-12-04 21:03:59 +0000

    update

---
+++ modules/user/zsh.nix
@@ -0,0 +1,65 @@
+{ config, lib, pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    shellAliases = { ll = "ls -l"; };
+    enableCompletion = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting.enable = true;
+    syntaxHighlighting.highlighters = [ "main" "brackets" "cursor" "line" ];
+    histFile = "$XDG_CACHE_HOME/zhistory";
+    histSize = 100000;
+    interactiveShellInit = ''
+      source "$(${pkgs.fzf}/bin/fzf-share)/key-bindings.zsh"
+    '';
+    ohMyZsh = {
+      enable = true;
+      plugins = [
+        "git"
+        "aliases"
+        "sudo"
+        "direnv"
+        "emacs"
+        "emoji"
+        "encode64"
+        "jsontools"
+        "systemd"
+        "dirhistory"
+        "colored-man-pages"
+        "command-not-found"
+        "extract"
+        "nix"
+      ];
+      customPkgs = with pkgs; [ nix-zsh-completions ];
+    };
+  };
+
+  programs.starship = {
+    enable = true;
+    settings = {
+      add_newline = false;
+      format = "$all";
+    };
+  };
+
+  environment.sessionVariables = {
+    SHELL = "${pkgs.zsh}/bin/zsh";
+    ZDOTDIR = "$XDG_CONFIG_HOME/zsh";
+    ZSH_CACHE = "$XDG_CACHE_HOME/zsh";
+    ZGEN_DIR = "$XDG_DATA_HOME/zsh";
+    ZGEN_SOURCE = "$ZGEN_DIR/zgen.zsh";
+  };
+
+  os.user.packages = with pkgs; [
+    zsh
+    nix-zsh-completions
+    fasd
+    fd
+    fzf
+    tldr
+    ripgrep
+    lolcat
+    screenfetch
+  ];
+}
