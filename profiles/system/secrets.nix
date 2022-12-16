2ecacda195d529f6de8ee671f636902b3ddc168c
padraic <patrick.morris.310@gmail.com>
2022-12-16 00:58:22 +0000

    fixes

--- profiles/system/secrets.nix
+++ profiles/system/secrets.nix
@@ -3,7 +3,8 @@
 {
 
   environment.systemPackages = with pkgs; [ age sops ssh-to-age ];
-  sops.defaultSopsFile = ../../hosts + "/${config.networking.hostName}.yaml";
+  sops.defaultSopsFile = ../../hosts
+    + "/${config.networking.hostName}/secrets.yaml";
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   # user secret will always be needed
